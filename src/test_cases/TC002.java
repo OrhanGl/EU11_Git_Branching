32c97f562136d759aea0dbda293dadc42dd0e383
OrhanGl <orhanglsrn13@gmail.com>
2023-06-30 13:09:43 +0300

    Created TC002

---
+++ src/test_cases/TC002.java
@@ -0,0 +1,18 @@
+package test_cases;
+
+public class TC002 {
+    public static void main(String[] args) {
+
+
+        //TC002
+
+        //Change to your life
+
+        //option to choose
+
+
+
+
+
+    }
+}
